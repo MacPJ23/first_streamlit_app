5a481517ba5344e426e1a9d422ff57ca93b6c227
Patrick <117084402+MacPJ23@users.noreply.github.com>
2022-11-02 10:43:05 +0100

    Update streamlit_app.py
    fixed 2nd picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # Alow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding jackfruit ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding jackfruit ', add_my_fruit)
